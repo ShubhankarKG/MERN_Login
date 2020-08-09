94d59321c01a7db404221141ea413988743a4f4d
ShubhankarKG <shubhankar.gupto.11@gmail.com>
2020-08-09 20:11:33 +0530

    refactor 1

--- views/src/components/Signin.jsx
+++ views/src/components/Signin.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import {
+  InputLabel,
+  Input,
+  Avatar,
+  Button,
+  CssBaseline,
+  FormControlLabel,
+  Checkbox,
+  Grid,
+  Typography,
+  Container,
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -68,61 +71,62 @@ export default function SignIn({ setToken }) {
           Sign in
         </Typography>
         <form className={classes.form}>
-          <section>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={values.email}
-              onChange={handleChange("email")}
-            />
-          </section>
-          <section>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={values.password}
-              onChange={handleChange("password")}
-            />
-          </section>
-          <section>
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-          </section>
-
-          <Button
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick={handleSubmit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <InputLabel htmlFor="email">Email Address</InputLabel>
+              <Input
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={values.email}
+                onChange={handleChange("email")}
+              />
             </Grid>
-            <Grid item>
-              {"Don't have an account?"}
-              <Link to="/signup"> Sign Up</Link>
+            <Grid item xs={12}>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={values.password}
+                onChange={handleChange("password")}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+                onClick={handleSubmit}
+              >
+                Sign In
+              </Button>
+            </Grid>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                {"Don't have an account?"}
+                <Link to="/signup"> Sign Up</Link>
+              </Grid>
             </Grid>
           </Grid>
         </form>
